f18e58b2c9afe968d51cfb83aa4db9fc7ea02b61
NayanthaNethsara <nayanthanethsara@gmail.com>
2025-09-24 22:24:34 +0530

    feat: add CoverImagePath and TitleSlug columns to books table in migration

---
+++ backend/BookService/Migrations/20250924165306_AddCoverUrlToBook.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoverUrlToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cover_image",
+                table: "books",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "title_slug",
+                table: "books",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cover_image",
+                table: "books");
+
+            migrationBuilder.DropColumn(
+                name: "title_slug",
+                table: "books");
+        }
+    }
+}
