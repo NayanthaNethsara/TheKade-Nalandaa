a4b7347f825510952a5d43e4a27caac4ead8c568
NayanthaNethsara <nayanthanethsara@gmail.com>
2025-10-24 13:00:31 +0530

    refactor: remove automatic database migration on startup

--- backend/AuthService/Program.cs
+++ backend/AuthService/Program.cs
@@ -1,7 +1,6 @@
 using AuthService.Configurations;
 using DotNetEnv;
 using AuthService.Data;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,10 +41,6 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
-    
-    // Apply pending migrations automatically on startup
-    await db.Database.MigrateAsync();
-    
     await DbSeeder.SeedAdminAsync(db);
 }
 
